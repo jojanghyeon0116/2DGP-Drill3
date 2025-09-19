da1294eac0b94e966fb2b061fbb44bd7c90596c6
Jo JangHyen(home) <pa7333@gmail.com>
2025-09-19 19:30:34 +0900

    top line 완성

--- character_moves.py
+++ character_moves.py
@@ -5,18 +5,24 @@ open_canvas()
 boy = load_image('character.png')
 
 def move_top():
+    print('Moving top')
+    for x in range(0, 800, 5):
+        draw_boy(x, 550)
     pass
 
 
 def move_right():
+    print('Moving right')
     pass
 
 
 def move_bottom():
+    print('Moving bottom')
     pass
 
 
 def move_left():
+    print('Moving left')
     pass
 
 def move_rectangle():
@@ -34,13 +40,16 @@ def move_circle():
     for deg in range(0, 360):
         x = r * math.cos(math.radians(deg)) + 400
         y = r * math.sin(math.radians(deg)) + 300
-        clear_canvas_now()
-        boy.draw_now(x, y)
-        delay(0.1)
+        draw_boy(x, y)
     pass
 
+def draw_boy(x: float, y: float):
+    clear_canvas_now()
+    boy.draw_now(x, y)
+    delay(0.1)
+
 while True:
-    move_circle()
+    # move_circle()
     move_rectangle()
     break
     pass
